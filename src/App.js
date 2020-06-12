05bf570cc1e9f8ce094318bbffaa93540afd140f
BraianBiale <braianbiale@gmail.com>
2020-06-12 20:33:18 -0300

    sape

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Maxi pollera, te fuiste y nos dejaste solos en pleno free weekend.
+          Maxi pollera, te fuiste y nos dejaste solos en pleno free weekend!.
         </p>
         <a
           className="App-link"

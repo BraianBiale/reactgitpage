c7f6b7279a021b3d51f8ac7ea550695135ab4eca
BraianBiale <braianbiale@gmail.com>
2020-06-12 20:25:45 -0300

    Updates

---
+++ precache-manifest.7461a1952fb4e907e21d22abab784dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f5ab01a8df7d44c42116018a6d7e06",
+    "url": "/reactgitpage/index.html"
+  },
+  {
+    "revision": "e61aa6dff911b56199cd",
+    "url": "/reactgitpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "00aff8c6cbc3a67de1b5",
+    "url": "/reactgitpage/static/js/2.aa386014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactgitpage/static/js/2.aa386014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61aa6dff911b56199cd",
+    "url": "/reactgitpage/static/js/main.b2dd5c42.chunk.js"
+  },
+  {
+    "revision": "a773a92f900c5f8c8c26",
+    "url": "/reactgitpage/static/js/runtime-main.6199b479.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactgitpage/static/media/logo.5d5d9eef.svg"
+  }
+]);

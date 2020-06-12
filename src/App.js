862ab88a2ccea31711ba29ead9a6898bca46fd77
BraianBiale <braianbiale@gmail.com>
2020-06-12 20:29:50 -0300

    change p element

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Maxi pollera, te fuiste y nos dejaste solos en pleno free weekend.
         </p>
         <a
           className="App-link"
